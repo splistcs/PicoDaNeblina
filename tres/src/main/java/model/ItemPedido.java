b185a28db93e0f0c87983ba6b6fbe74d12be790c
Splist <icarohamonpmarchiori@gmail.com>
2026-05-15 14:05:19 -0300

    new file:   tres/src/main/java/model/ItemCarrinho.java 	new file:   tres/src/main/java/model/ItemPedido.java 	new file:   tres/target/classes/model/ItemCarrinho.class 	new file:   tres/target/classes/model/ItemProduto.class 	deleted:    tres/target/classes/model/ItemProduto.class

---
+++ tres/src/main/java/model/ItemPedido.java
@@ -0,0 +1,51 @@
+package model;
+
+import java.math.BigDecimal;
+
+public class ItemPedido {
+    private int id;
+    private Sku sku;
+    private int quantidade;
+    private BigDecimal precoUnitario;
+
+    public ItemPedido() {
+    }
+
+    public BigDecimal calcularPrecoTotal(){
+        return this.precoUnitario.multiply(BigDecimal.valueOf(this.quantidade));
+    }
+
+
+    /* getters e setters */
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Sku getSku() {
+        return sku;
+    }
+
+    public void setSku(Sku sku) {
+        this.sku = sku;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = BigDecimal.valueOf(precoUnitario);
+    }
+}
