ca225091d09cea7634082a554403df337a754059
lucas cho <lucas.lupo2003@gmail.com>
2026-05-17 16:34:47 -0300

    Implementação do PesquisarProduto ctl
    testarei agora tudo e vejamos no que vai dar...

--- tres/src/main/java/ctl/ctlPesquisarProduto.java
+++ tres/src/main/java/ctl/ctlPesquisarProduto.java
@@ -20,5 +20,52 @@
  *                    INNER JOIN produto ON (id_produto = id_prod)
  *    WHERE (id_cat = ?) AND (nome_prod LIKE %?%)
  *
- *
  */
+package ctl;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import dao.PesquisarProdutoDao;
+import model.Produto;
+
+public class ctlPesquisarProduto {
+  private List<Produto> produtos = new ArrayList<>();
+
+  private void imprimirProduto() {
+    System.out.println("| id_prod | nome_prod | descricao | material | marca | ativo | pwd_img |");
+    for (Produto ref : this.produtos) {
+      System.out.printf("| %d | %s | %s | %s | %s | %b | %s |\n",
+        ref.getId(), ref.getNome(), ref.getDescricao(), ref.getMaterial(), ref.getMarca(), ref.isAtivo(), ref.getImagemPrincipalUrl());
+    }
+  }
+
+  public boolean pesquisar (int idCategoria, String nome) throws SQLException {
+
+    this.produtos.clear();
+    PesquisarProdutoDao pesquisarProdutoDao = new PesquisarProdutoDao();
+
+    if(nome != null && idCategoria > 0) {
+      if(pesquisarProdutoDao.buscarPorNomeECategoria(nome, idCategoria, this.produtos)) {
+        imprimirProduto();
+        return true;
+      }
+      return false;
+    } else if (nome != null) {
+      if(pesquisarProdutoDao.buscarPorNome(nome, this.produtos)) {
+        imprimirProduto();
+        return true;
+      }
+      return false;
+    } else if (idCategoria > 0) {
+      if(pesquisarProdutoDao.buscarPorCategoria(idCategoria, this.produtos)) {
+        imprimirProduto();
+        return true;
+      }
+      return false;
+    } else {
+      return false;
+    }
+  }
+}
