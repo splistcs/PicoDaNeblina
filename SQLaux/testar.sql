d565958a3cb42a82206c9a71ab0516183ce70142
lucas cho <lucas.lupo2003@gmail.com>
2026-05-24 23:56:16 -0300

    SQL teste melhorado

--- SQLaux/testar.sql
+++ SQLaux/testar.sql
@@ -6,9 +6,9 @@ VALUES  ('Masculino', 'masc', true, NULL),
         ('Calças', 'calcas', true, 2);
 
 INSERT INTO produto (nome_prod, descricao, material, marca, ativo, pwd_img)
-VALUES  ('Camisa Polo', 'Polo confortavel para o dia a dia.', 'Algodao', 'Renner', true, 'img/polo.jpg'),
-        ('Calca Jeans', 'Calca jeans slim fit com elastano.', 'Jeans', 'Levis', true, 'img/jeans.jpg'),
-        ('Cueca Boxer', 'Cueca boxer sem costura super confortavel.', 'Microfibra', 'SiofStyle', true, 'img/boxer.jpg');
+VALUES  ('Camisa Polo', 'Polo confortavel para o dia a dia.', 'Algodao', 'Renner', true, 'teste1.jpg'),
+        ('Calca Jeans', 'Calca jeans slim fit com elastano.', 'Jeans', 'Levis', true, 'teste1.jpg'),
+        ('Cueca Boxer', 'Cueca boxer sem costura super confortavel.', 'Microfibra', 'SiofStyle', true, 'teste1.jpg');
 
 INSERT INTO tem_categoria (id_produto, id_categoria)
 VALUES  (1, 1),
@@ -16,6 +16,9 @@ VALUES  (1, 1),
         (2, 2),
         (2, 5),
         (3, 4);
+
+INSERT INTO cliente(nome_completo, email, telefone, cpf, senhaHash, ativo, tipoCadastro)
+VALUES ('Linguinus', 'ling@gmail.com', '1898', '192921', '1234', true, 2);
 
 SELECT * FROM categoria;
 SELECT nome_prod, nome_cat FROM
